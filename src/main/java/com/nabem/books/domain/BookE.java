d646140a574b7b07fcbab18c3e821dfa2abc246f
Nabemh <nhanior@gmail.com>
2024-12-20 18:23:59 +0100

    files initialized

---
+++ src/main/java/com/nabem/books/domain/BookE.java
@@ -0,0 +1,17 @@
+package com.nabem.books.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class BookE {
+    
+    private String author;
+    
+}
